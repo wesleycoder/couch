4553e2aa7226b749a1db1a696f000b081119bb33
Wésley Guimarães <wes@guima.dev>
2024-05-13 10:47:43 -0300

    feat: tmdb api token & type

--- pkgs/tmdb/index.ts
+++ pkgs/tmdb/index.ts
@@ -1,8 +1,11 @@
 import env from '@pkgs/env'
+import type { Movie } from '@pkgs/turso-db'
 
 export async function getMovieDetails(id: number) {
-  const response = await fetch(`${env.TMDB_API_URL}/movie/${id}`)
+  const response = await fetch(`${env.TMDB_API_URL}/movie/${id}`, {
+    headers: { Authorization: `Bearer ${env.TMDB_ACCESS_TOKEN}` },
+  })
   if (!response.ok) throw new Error(response.statusText)
 
-  return await response.json()
+  return (await response.json()) as Movie
 }
