6846ec89545f7e7f30a05d98a7e13d063cc28f83
Wésley Guimarães <wes@guima.dev>
2024-05-13 10:49:53 -0300

    feat: import and save to astra and turso

--- pkgs/astra-db/index.ts
+++ pkgs/astra-db/index.ts
@@ -1,21 +1,95 @@
-import { DataAPIClient, type VectorDoc } from '@datastax/astra-db-ts'
+import type { CreateCollectionOptions, SomeDoc } from '@datastax/astra-db-ts'
 import env from '@pkgs/env'
-import type { Movie } from '@pkgs/turso-db'
 
-const client = new DataAPIClient(env.ASTRA_DB_APPLICATION_TOKEN, {
-  dbOptions: {},
-})
+const baseEndpoint = `${env.ASTRA_DB_API_ENDPOINT}/api/json/v1/${env.ASTRA_DB_KEYSPACE}`
+const defaultFetchOptions = {
+  method: 'POST',
+  headers: {
+    Accept: 'application/json',
+    'Content-Type': 'application/json',
+    Token: env.ASTRA_DB_APPLICATION_TOKEN,
+  },
+}
 
-export const astraDB = client.db(env.ASTRA_DB_API_ENDPOINT)
+const defaultCollectionOptions: CreateCollectionOptions<object> = {
+  defaultId: {
+    type: 'objectId',
+  },
+  indexing: { allow: ['*'] },
+  vector: {
+    dimension: env.OPENAI_DEFAULT_EMBED_DIMENSIONS,
+    metric: 'cosine',
+  },
+}
 
-console.log(`* Connected to AstraDB ${astraDB.id}`)
+export const astraClient = {
+  async getColection(collectionName: string) {
+    const response = await fetch(baseEndpoint, {
+      ...defaultFetchOptions,
+      body: JSON.stringify({
+        findCollections: {
+          options: {
+            explain: true,
+          },
+        },
+      }),
+    })
 
-export const movieCollection = await astraDB.createCollection<Movie & VectorDoc>('tmdb_movies')
+    if (!response.ok) await astraClient.createCollection(collectionName)
 
-console.log(`* Created collection ${movieCollection.namespace}.${movieCollection.collectionName}`)
+    const res = await response.json()
 
-process.addListener('beforeExit', () => {
-  console.log('* Closing connection to AstraDB...')
-  client.close()
-  console.log('* Bye!')
-})
+    const collection = res?.status?.collections?.find?.((c: { name: string }) => c?.name === collectionName) as
+      | { name: string }
+      | undefined
+
+    return collection ?? (await astraClient.createCollection(collectionName))
+  },
+  async createCollection<T extends SomeDoc>(
+    collectionName: string,
+    options: CreateCollectionOptions<T> = defaultCollectionOptions,
+  ) {
+    const response = await fetch(baseEndpoint, {
+      ...defaultFetchOptions,
+      body: JSON.stringify({
+        createCollection: {
+          name: collectionName,
+          options: {
+            ...defaultCollectionOptions,
+            ...options,
+          },
+        },
+      }),
+    })
+
+    if (!response.ok) return
+
+    const res = await response.json()
+
+    if (!res?.status?.ok) return
+
+    return { name: collectionName }
+  },
+  async insertOne<T extends object>(collectionName: string, document: T) {
+    const response = await fetch(`${baseEndpoint}/${collectionName}`, {
+      ...defaultFetchOptions,
+      body: JSON.stringify({ insertOne: { document } }),
+    })
+
+    if (!response.ok) throw new Error("Couldn't insert document")
+
+    const res = await response.json()
+
+    return res?.status?.insertedIds?.[0]?.$objectId ?? (res?.status?.insertedIds?.[0] as string | undefined)
+  },
+  async insertMany<T extends object>(collectionName: string, documents: T[]) {
+    const response = await fetch(`${baseEndpoint}/${collectionName}/batch`, {
+      ...defaultFetchOptions,
+      body: JSON.stringify({ insertMany: { documents } }),
+    })
+
+    if (!response.ok) throw new Error("Couldn't insert documents")
+
+    return response.json()
+  },
+}
--- pkgs/import/index.ts
+++ pkgs/import/index.ts
@@ -1,46 +1,87 @@
+import type { VectorDoc } from '@datastax/astra-db-ts'
 import { getEmbedding } from '@pkgs/ai'
-import { movieCollection } from '@pkgs/astra-db'
+import { astraClient } from '@pkgs/astra-db'
 import { getMovieDetails } from '@pkgs/tmdb'
-import { db, moviesTable } from '@pkgs/turso-db'
+import {
+  db,
+  moviesTable,
+  type Genre,
+  type Movie,
+  type ProductionCompanies,
+  type ProductionCountries,
+  type SpokenLanguages,
+} from '@pkgs/turso-db'
+import { eq } from 'drizzle-orm'
 import { parseArgs } from 'node:util'
 import { z } from 'zod'
 
-if (import.meta.main) {
-  const argsSchema = z.object({
-    movieId: z.coerce.number().int().positive(),
-  })
+type MovieVectorProps =
+  | 'id'
+  | 'imdb_id'
+  | 'original_title'
+  | 'overview'
+  | 'title'
+  | 'runtime'
+  | 'tagline'
+  | 'status'
+  | 'vote_average'
+  | 'vote_count'
+  | 'revenue'
+  | 'release_date'
+  | 'popularity'
+  | 'budget'
 
-  const args = parseArgs({
-    args: Bun.argv,
-    options: {
-      movieId: {
-        type: 'string',
-      },
-    },
-    strict: true,
-    allowPositionals: true,
-  })
+interface MovieVector extends Pick<Movie, MovieVectorProps>, VectorDoc {
+  belongs_to_collection: string | null
+  genres: Genre[] | null
+  production_companies: ProductionCompanies[] | null
+  production_countries: ProductionCountries[] | null
+  spoken_languages: SpokenLanguages[] | null
+  combined: string
+}
 
-  args.positionals.shift()
-  args.positionals.shift()
+const moviesCollectionName = 'tmdb_movies'
 
-  const saneArgs = {
-    movieId: args.values.movieId ?? args.positionals.at(0),
-  }
+export const updateMovieVectors = async (movie: Movie) => {
+  const moviesCollection = await astraClient.getColection(moviesCollectionName)
+  if (!moviesCollection) throw new Error('Collection not found in astra')
 
-  const { movieId } = argsSchema.parse(saneArgs)
-
-  const movie = await getMovieDetails(movieId)
-
-  await db.insert(moviesTable).values([movie]).run()
-
-  const $vector = await getEmbedding(`
+  const combined = `
     $title: ${movie.title}
     $genres: ${JSON.stringify(movie.genres ?? [])}
     $release_date: ${movie.release_date}
     $tagline: ${movie.tagline}
     $overview: ${movie.overview}
-  `)
+  `
+  const $vector = await getEmbedding(combined)
+  const astraId = await astraClient.insertOne<MovieVector>(moviesCollectionName, { ...movie, combined, $vector })
+  console.log('Inserted movie into astra collection', astraId)
 
-  await movieCollection.insertOne({ ...movie, $vector })
+  const res = await db.update(moviesTable).set({ vector: $vector, astraId }).returning({ id: moviesTable.id }).run()
+  console.log('Saved vector to turso db', res.rows.at(0)?.id)
+}
+
+export const importFromMovieId = async (movieId: number) => {
+  const dbMovie = await db.query.movies.findFirst({ where: eq(moviesTable.id, movieId) })
+  if (dbMovie) return
+
+  const movie = await getMovieDetails(movieId)
+
+  const res = await db.insert(moviesTable).values([movie]).returning({ id: moviesTable.id }).run()
+  console.log('Saved movie to turso db', res.rows.at(0)?.id)
+}
+
+if (import.meta.main) {
+  const argsSchema = z.object({ movieId: z.coerce.number().int().positive() })
+
+  const args = parseArgs({
+    args: Bun.argv,
+    options: { movieId: { type: 'string' } },
+    strict: true,
+    allowPositionals: true,
+  })
+
+  const { movieId } = argsSchema.parse({ movieId: args.values.movieId ?? args.positionals.at(2) })
+
+  await importFromMovieId(movieId)
 }
